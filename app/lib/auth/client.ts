47417e76f0ec341806518b947773d794c55a1449
Otakukorean <bboomstation1@gmail.com>
2024-12-16 23:52:49 +0300

    new45

--- app/lib/auth/client.ts
+++ app/lib/auth/client.ts
@@ -1,7 +1,7 @@
 import { adminClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 export const {signIn,signOut,signUp,useSession,user} = createAuthClient({
-    baseURL: "https://6760872e9b9c486680a8bb6a--tranquil-cupcake-4f32e9.netlify.app" ,// the base url of your auth server ,
+    baseURL: "https://nova-9z4088yra-tgrdg.vercel.app" ,// the base url of your auth server ,
     plugins :[
         adminClient()
     ]
