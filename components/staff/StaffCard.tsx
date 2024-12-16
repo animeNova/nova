b7c233c9453a5b9035fde901564e81d867991be9
Otakukorean <bboomstation1@gmail.com>
2024-12-17 01:59:37 +0300

    new474689

--- components/staff/StaffCard.tsx
+++ components/staff/StaffCard.tsx
@@ -16,7 +16,7 @@ const StaffCard : React.FC<StaffCardProps> = ({id,job,name,image}) => {
     className='relative transition cursor-pointer rounded-lg hover:shadow-[0_0_30px_rgba(167,139,250,0.9)]'
     > 
     <div>
-        <Image src={image} width={1000} height={1000}  alt='' className='w-[180px] h-[260px] md:w-[220px] md:h-[300px] rounded-md object-cover ' />
+        <Image src={image || '/assets/unknown.jpg'} width={1000} height={1000}  alt='' className='w-[180px] h-[260px] md:w-[220px] md:h-[300px] rounded-md object-cover ' />
     </div>
     <div className='absolute bottom-4 left-4 '>
       <h1 className={cn( 'text-lg font-normal transition text-white/75 hover:text-white')}>{name}</h1>
