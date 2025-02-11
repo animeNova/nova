831e3807803d0c092c17fce07edd433c2b576394
Otakukorean <bboomstation1@gmail.com>
2025-02-11 19:07:13 +0300

    edit embedding

--- app/(root)/(pages)/genre/[id]/page.tsx
+++ app/(root)/(pages)/genre/[id]/page.tsx
@@ -2,7 +2,7 @@ import AnimeCard from '@/components/cards/AnimeCard'
 import OrderbyFilter from '@/components/ui/orderbyFilter'
 import { db } from '@/drizzle'
 import { genre, show, showToGenre } from '@/drizzle/db/schema'
-import { and, eq, sql } from 'drizzle-orm'
+import { and, eq } from 'drizzle-orm'
 import React from 'react'
 
 interface PageParams {
@@ -13,16 +13,16 @@ interface PageParams {
 
 const page : React.FC<PageParams> =async ({params}) => {
     const {id} = await params
-    const result = await db.execute(sql`
-        SELECT DISTINCT s.id, s.title , s.image , s.video , s.airing
-        FROM show_genre sg
-        JOIN Show s ON sg.show_id = s.id
-        WHERE sg.genre_id = ${id};
-        `)
+    const result = await db.select({
+        id : show.id,
+        title : show.title,
+        image : show.image,
+        airing : show.airing,
+        video : show.video
+    }).from(show).innerJoin(showToGenre,and(eq(showToGenre.showId,show.id),eq(showToGenre.genreId,id)))
     const genreData = await db.select({
         title :genre.title
     }).from(genre).where(eq(genre.id,id))
- 
   return (
     <div>
       <div className='flex justify-between items-center'>
@@ -37,8 +37,8 @@ const page : React.FC<PageParams> =async ({params}) => {
      </div>
       <div className='flex-wrap flex justify-center items-center gap-4 md:gap-14'>
       {
-        result?.rows?.map((show) => (
-          <AnimeCard id={show.id!} image={show.image} video={show.video} title={show.title} year={show.airing} key={show.id}  />
+        result.map((show) => (
+          <AnimeCard id={show.id} image={show.image} video={show.video} title={show.title} year={show.airing} key={show.id}  />
         ))
       }
     </div>
